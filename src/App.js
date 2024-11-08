40d53dec33152850d74db90da5f676d1cef9b03d
boshra <boshra@gmail.com>
2024-11-08 22:17:44 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { Box, Stack } from "@mui/material";
+import "./App.css";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      this is project demo 
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+      </Stack>
+      </Box>
     </div>
   );
 }
